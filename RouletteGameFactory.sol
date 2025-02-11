2ea458f8017285a3c0aec7ca57c3d1be25c59192
John Kwok <jk.johnkwok@gmail.com>
2025-02-11 05:13:24 +0000

    update version

--- RouletteGameFactory.sol
+++ RouletteGameFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
+pragma solidity >=0.8.2 <0.9.0;
 import "./RouletteGame.sol";
 contract RouletteGameFactory {
     RouletteGame[] private _rouletteGames;
